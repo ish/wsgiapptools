72547d7462b80d345f67df5c4f74d315b5f7eb26
Matt Goodall <matt@jdi-associates.com>
2008-12-22 16:35:54 +0000

    Create a Flash messages manager and then hide its API behind module-level functions that all work on a WSGI environ.

--- wsgiapptools/flash.py
+++ wsgiapptools/flash.py
@@ -5,10 +5,12 @@ A "flash" message is a message displayed on a web page that is removed next
 request.
 """
 
-__all__ = ['add_message', 'get_messages', 'flash_middleware_factory']
+__all__ = ['add_message', 'get_messages', 'get_flash', 'flash_middleware_factory']
 
 
 import itertools
+import webob
+
 from wsgiapptools import cookies
 
 
@@ -16,39 +18,67 @@ ENVIRON_KEY = 'wsgiapptools.flash'
 COOKIE_NAME = 'flash'
 
 
-def add_message(request, message, type=None):
+def add_message(environ, message, type=None):
     """
-    Add a new flash message.
-
-    Note: this can be called multiple times to set multiple messages. The
-    messages can be retrieved, using get_messages below, and will be returned
-    in the order they were added.
+    Add the flash message to the Flash manager in the WSGI environ."
     """
-    if type is None:
-        type = ''
-    flash = '%s:%s' % (type, message)
-    request.environ.setdefault(ENVIRON_KEY, []).append(flash)
+    return get_flash(environ).add_message(message, type)
 
 
-def get_messages(request):
+def get_messages(environ):
     """
-    Retrieve flash messages found in the request's cookies, returning them as a
-    list of (type, message) tuples and deleting the cookies.
+    Get the flasg messages from the Flash manager in the WSGI environ.
     """
-    messages = []
-    cookie_cutter = cookies.get_cookie_cutter(request.environ)
-    for i in itertools.count():
-        cookie_name = '%s.%d'%(COOKIE_NAME, i)
-        # Try to find the next message. Leave the loop if it does not exist.
-        message = request.cookies.get(cookie_name)
-        if message is None:
-            break
-        # Remove the cookie, presumably it will be displayed shortly.
-        cookie_cutter.delete_cookie(cookie_name)
-        # Parse  and yield the message.
-        type, message = message.split(':', 1)
-        messages.append((type or None, message))
-    return messages
+    return get_flash(environ).get_messages()
+
+
+def get_flash(environ):
+    """
+    Get the flash manager from the environ.
+    """
+    return environ[ENVIRON_KEY]
+
+
+class Flash(object):
+    """
+    Flash message manager, associated with a WSGI environ.
+    """
+
+    def __init__(self, environ):
+        self.request = webob.Request(environ)
+        self.flashes = []
+
+    def add_message(self, message, type=None):
+        """
+        Add a new flash message.
+
+        Note: this can be called multiple times to set multiple messages. The
+        messages can be retrieved, using get_messages below, and will be returned
+        in the order they were added.
+        """
+        if type is None:
+            type = ''
+        self.flashes.append('%s:%s'%(type, message))
+
+    def get_messages(self):
+        """
+        Retrieve flash messages found in the request's cookies, returning them as a
+        list of (type, message) tuples and deleting the cookies.
+        """
+        messages = []
+        cookies_mgr = cookies.get_cookies(self.request.environ)
+        for i in itertools.count():
+            cookie_name = '%s.%d'%(COOKIE_NAME, i)
+            # Try to find the next message. Leave the loop if it does not exist.
+            message = self.request.cookies.get(cookie_name)
+            if message is None:
+                break
+            # Remove the cookie, presumably it will be displayed shortly.
+            cookies_mgr.delete_cookie(cookie_name)
+            # Parse  and yield the message.
+            type, message = message.split(':', 1)
+            messages.append((type or None, message))
+        return messages
 
 
 def flash_middleware_factory(app):
@@ -60,11 +90,13 @@ def flash_middleware_factory(app):
         def _start_response(status, response_headers, exc_info=None):
             # Iterate the new flash messages in the WSGI, setting a 'flash'
             # cookie for each one.
-            cookie_cutter = cookies.get_cookie_cutter(environ)
-            for i, flash in enumerate(environ.get(ENVIRON_KEY, [])):
-                cookie_cutter.set_cookie(('%s.%d'%(COOKIE_NAME,i), flash))
+            flash = environ[ENVIRON_KEY]
+            cookies_mgr = cookies.get_cookies(environ)
+            for i, flash in enumerate(flash.flashes):
+                cookies_mgr.set_cookie(('%s.%d'%(COOKIE_NAME,i), flash))
             # Call wrapped app's start_response.
             return start_response(status, response_headers, exc_info)
+        environ[ENVIRON_KEY] = Flash(environ)
         return app(environ, _start_response)
     return middleware
 
